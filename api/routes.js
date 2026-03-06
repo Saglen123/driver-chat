5920f66cddfc85b41ce3250066f87ec20c97a0c4
Saglen123 <sindre.aglen@hotmail.com>
2026-03-06 12:40:29 +0100

    Fix formatting issues in getSheet function

--- api/routes.js
+++ api/routes.js
@@ -4,16 +4,16 @@ async function getSheet(sheetName) {
   const auth = new google.auth.GoogleAuth({
     credentials: {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     },
-    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
 
   const sheets = google.sheets({ version: "v4", auth });
 
   const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.SHEET_ID, 
-    range: sheetName
+    spreadsheetId: process.env.SHEET_ID,
+    range: sheetName,
   });
 
   return res.data.values || [];
